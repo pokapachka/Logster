61ffc9dcd3ac1e88b058128b987723cb7998a45f
pokapachka <pachkaosokin@yandex.ru>
2025-04-11 16:11:30 +0500

    создание основной страницы

--- app/src/main/java/com/example/logster/MainActivity.java
+++ app/src/main/java/com/example/logster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.logster;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window w = getWindow();
+        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
